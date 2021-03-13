6889fcab733349933d23bdc4e9685e8ea002ef01
_blockaye_ <blake_austinjohnson@icloud.com>
2021-03-13 09:54:16 -0600

    debugging quiz start

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,33 @@
 
 var startButton = document.getElementById("start-quiz");
 
-var QandA = {
-    q1: ["A-answer", "B-answer", "C-answer", "D-answer"],
-    q2: ["A-answer", "B-answer", "C-answer", "D-answer"],
-    q3: ["A-answer", "B-answer", "C-answer", "D-answer"]
-};
+var QandA = [
+    {
+      q: "this is question 1?",
+      ch: ["A-answer", "B-answer", "C-answer", "D-answer"],
+      a: "a"
+    }, 
+    {
+      q: "this is question 2?",
+      ch: ["A-answer", "B-answer", "C-answer", "D-answer"],
+      a: "b"
+    },
+    {
+      q: "this is question 3?",
+      ch: ["A-answer", "B-answer", "C-answer", "D-answer"],
+      a: "c"
+    },
+    {
+      q: "this is question 4?",
+      ch: ["A-answer", "B-answer", "C-answer", "D-answer"],
+      a: "d"
+    }
+  ];
+console.log(QandA);
 
+var i = 0;
+
+//timer function
 function countdown() {
     var timeDisplay = document.getElementById("timer");
     var time = 60;
@@ -20,8 +41,99 @@ function countdown() {
         timeDisplay.innerHTML =  "Time: " + time;
       }
     }
-  };
+};
+
+
+
+function startQuiz() {
+  debugger;
+  
+  //how do i access the h1 element in the div for questions?
+  let questionEl = document.getElementById("question");
+  
+  let choiceEl = document.getElementsByClassName("choice-container");
+  
+  //will this remove or clear all the child elements in the choice div so its blank every time
+  //a new set of choices is loaded to the screen?
+  // while (choiceEl.firstChild) {
+  //   choiceEl.removeChild(choiceEl.firstChild);
+  // };
+  
+  let currentQuestion = QandA[i].q;
+  questionEl.innerHTML = currentQuestion;
+  console.log(currentQuestion);
+
+  let currentChoices = QandA[i].ch;
+  console.log(currentChoices);
+  
+  //let choice = currentChoices[i];
+  //console.log(choice);
+
+  currentChoices.forEach(function(choice, i ) {
+    let chB = document.createElement("button");
+    chB.setAttribute("value", choice);
+    chB.textContent = i + 1 + "." + choice;
+    console.log(chB);
+    choiceEl.appendChild(chB);
+    i + 1;
+  });
+  
+
+};
 
 startButton.addEventListener("click", countdown);
+startButton.addEventListener("click", startQuiz);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// function quizLayout()  {
+//   let questionPlacement = document.getElementsByTagName("h1");
+//   let choicePlacement = document.getElementsByClassName("choice-content");
+
+//   //questionPlacement.innerHTML(QandA[x].q)
+
+//   for (var i = 0; i < QandA.length; i ++) {
+//     let choice1 = document.createElement("button").innerHTML(QandA[i].ch[i]);
+//     let choice2 = document.createElement("button").innerHTML(QandA[i].ch[i=+1]);
+//     let choice3 = document.createElement("button").innerHTML(QandA[i].ch[i=+2]);
+//     let choice4 = document.createElement("button").innerHTML(QandA[i].ch[i=+3]); 
+//     let choicesArr = [choice1, choice2, choice3, choice4];
+    
+//     choicePlacement.appendChild(choicesArr[i]);
+//   }
+
+  
+    
+
+
+//     console.log(questionPlacement);
+//     console.log(choicePlacement);
+//     console.log(choice1);
+//     console.log(choice2);
+//     console.log(choice3);
+//     console.log(choice4);
+ 
+
+    
+
+
+
+
+//     //question.innerHTML = QandA.q1
+
+    
+    
+// };
+
+// quizLayout();
